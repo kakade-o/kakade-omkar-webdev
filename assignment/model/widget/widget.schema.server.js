55950aba3d6ada5b1cb35dde772aba125e3a472f
omkarkakade <omkar710@gmail.com>
2017-08-07 15:00:48 -0400

    added widget-text

--- assignment/model/widget/widget.schema.server.js
+++ assignment/model/widget/widget.schema.server.js
@@ -1,16 +1,20 @@
 var mongoose = require("mongoose");
 var widgetSchema = mongoose.Schema({
-    _page: {type: mongoose.Schema.ObjectId, ref: "PageModel"},
-    type: {type: String, enum: ['HEADING', 'IMAGE', 'YOUTUBE', 'HTML', 'INPUT', 'TEXT']},
+    _page: {required: true, type: mongoose.Schema.ObjectId, ref: "PageModel"},
+    type: {required:true, type: String, enum: ['HEADING', 'IMAGE', 'YOUTUBE', 'HTML', 'INPUT', 'TEXT']},
     name: String,
     text: String,
     description: String,
     url: String,
     width: String,
+    placeholder: String,
     height: String,
-    size: String,
+    size: Number,
     class: String,
+    rows: Number,
+    icon: String,
     deletable: Boolean,
+    formatted: Boolean,
     dateCreated: {type: Date, default: Date.now}
 }, {collection: "widget"});
 
