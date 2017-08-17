1a38378a00237dd9f55e5f751a6628727ab82ba3
ravalese <ravalese.j@husky.neu.edu>
2017-08-16 21:35:26 -0400

    heroku google auth fix

--- project/services/user.service.server.js
+++ project/services/user.service.server.js
@@ -42,11 +42,11 @@ module.exports = function (app) {
     app.post("/api/project/logout", logout);
     app.post("/api/project/register", register);
     app.get("/auth/google", passport.authenticate('google', { scope : ['profile'] }));
-    app.get('http://kakade-omkar-webdev.herokuapp.com/auth/google/callback',
-        passport.authenticate('google', {
-            successRedirect: '/project/index.html#!/profile',
-            failureRedirect: '/project/index.html#!/login'
-        }));
+    // app.get('/auth/google/callback',
+    //     passport.authenticate('google', {
+    //         successRedirect: '/project/index.html#!/profile',
+    //         failureRedirect: '/project/index.html#!/login'
+    //     }));
 
     app.get('/auth/google/callback',
         passport.authenticate('google', {
